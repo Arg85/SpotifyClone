603693507236b6273a4e86afc0ddeafd79e3fc0c
Arg51~ <anuragy554@gmail.com>
2022-11-28 13:13:56 -0800

    worked on playpause functionality as well as top play and sidebar

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -2,15 +2,17 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
-const SongCard = ({ song, i }) => {
-  const activeSong="Test"
-  const handlePauseClick=()=>{
-    
-  }
-  const handlePlayClick=()=>{
-    
-  }
-  return(
+const SongCard = ({ song, i, activeSong, isPlaying, data }) => {
+  const dispatch = useDispatch();
+  const handlePauseClick = () => {
+    console.log("calledd");
+    dispatch(playPause(false));
+  };
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+  return (
     <div className="flex flex-col w-[250px] p-5 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer ">
       <div className="relative w-full h-56 group">
         <div
@@ -18,22 +20,34 @@ const SongCard = ({ song, i }) => {
             activeSong?.title === song.title ? "flex bg-black" : "hidden"
           }`}
         >
-          <playPause song={song} handlePause={handlePauseClick} handlePlay={handlePlayClick}/>
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
         </div>
-        <img alt="song_img" src={song.images?.coverart}/>
+        <img alt="song_img" src={song.images?.coverart} />
       </div>
       <div className="mt-4 flex flex-col">
         <p className="font-semibold text-lg text-white truncate">
-          <Link to={`/songs/${song?.key}`}>
-          {song.title}</Link>
+          <Link to={`/songs/${song?.key}`}>{song.title}</Link>
         </p>
         <p className="text-sm truncate text-gray-300 mt-1">
-          <Link to={song.artists?`/artists/${song?.artists[0].adamid}`:'top-artists'}>
-          {song.subtitle}</Link>
+          <Link
+            to={
+              song.artists
+                ? `/artists/${song?.artists[0].adamid}`
+                : "top-artists"
+            }
+          >
+            {song.subtitle}
+          </Link>
         </p>
       </div>
     </div>
   );
-}
+};
 
 export default SongCard;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,15 +1,17 @@
 import { genres } from "../assets/constants";
 import { Error, Loader, SongCard } from "../components";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { useDispatch, useSelector } from "react-redux";
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
-  console.log(data);
   const genreTitle = "Pop";
-  if(isFetching) return <Loader title="loading Songs"/>
-  if(error) return <Error/>
+  if (isFetching) return <Loader title="loading Songs" />;
+  if (error) return <Error />;
   return (
     <div className="flex flex-col">
-      <div className="w-full justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+      <div className=" flex w-full justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">
           Discover {genreTitle}
         </h2>
@@ -25,7 +27,16 @@ const Discover = () => {
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data.map((song, i) => {
-          return <SongCard key={song.key} song={song} i={i} />;
+          return (
+            <SongCard
+              key={song.key}
+              data={data}
+              song={song}
+              i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+            />
+          );
         })}
       </div>
     </div>
